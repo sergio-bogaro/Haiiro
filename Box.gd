609ee0ea79a60baf9607c3d07968d7f50ee963cd
Sergio Bogaro Rodrigues <sergiobogaro@gmail.com>
2022-02-24 11:29:07 -0300

    Mecanincas inicias Funcionando

---
+++ Box.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+onready var ray = $RayCast2D
+var grid_size = 16
+var inputs = {
+	'ui_up': Vector2.UP,
+	'ui_down': Vector2.DOWN,
+	'ui_left': Vector2.LEFT,
+	'ui_right': Vector2.RIGHT
+}
+
+func move(dir):
+	var vector_pos = inputs[dir] * grid_size
+	ray.cast_to = vector_pos
+	ray.force_raycast_update()
+	$Tween.interpolate_property(
+		self, "position",
+		position, position + vector_pos, 0.1,
+		Tween.TRANS_BACK,
+		Tween.EASE_IN_OUT
+	)
+	if (!ray.is_colliding()):
+		$Tween.start()
+		position += inputs[dir] * grid_size
+		return true
+	else:
+		var collider = ray.get_collider()
+		if collider.is_in_group('box'):
+			if (collider.move(dir)):
+				$Tween.start()
+				position += vector_pos
+				return true	
+		else:
+			return false
---
+++ Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+onready var ray = $RayCast2D
+var grid_size = 16
+var inputs = {
+	'ui_up': Vector2.UP,
+	'ui_down': Vector2.DOWN,
+	'ui_left': Vector2.LEFT,
+	'ui_right': Vector2.RIGHT
+}
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			if ($Tween.is_active() == false):
+				move(dir)
+
+func move(dir):
+	var vector_pos = inputs[dir] * grid_size
+	ray.cast_to = vector_pos
+	ray.force_raycast_update()
+	$Tween.interpolate_property(
+		self, "position",
+		position, position + vector_pos, 0.1,
+		Tween.TRANS_BACK,
+		Tween.EASE_IN_OUT
+	)
+	if (!ray.is_colliding()):
+		$Tween.start()
+		position += vector_pos
+	else:
+		var collider = ray.get_collider()
+		if collider.is_in_group('box'):
+			if (collider.move(dir)):
+				$Tween.start()
+				position += vector_pos
